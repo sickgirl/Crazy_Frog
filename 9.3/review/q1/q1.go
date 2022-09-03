aa59554f84df5a691348f1ff53b49aa7841b1dc0
mountain <hot007boy@126.com>
2022-09-03 09:42:52 +0800

    Initial commit

---
+++ 9.3/review/q1/q1.go
@@ -0,0 +1,24 @@
+package main
+
+//去除某个元素
+//https://leetcode.cn/problems/remove-element/
+
+import "fmt"
+
+func main() {
+	nums := []int{3, 2, 2, 3}
+	val := 2
+	r := removeElement(nums, val)
+	fmt.Printf("%v", r)
+}
+func removeElement(nums []int, val int) int {
+
+	for i := 0; i < len(nums); i++ {
+		if nums[i] == val {
+			nums = append(nums[:i], nums[i+1:]...)
+			i--
+		}
+	}
+	return len(nums)
+
+}
