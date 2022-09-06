10c4bfde3360a528f2f7cdfbe0342bd3c0554e7d
mountain <hot007boy@126.com>
2022-09-06 15:53:55 +0800

    上班卷

--- 9.6/q1/q1.go
+++ 9.6/q1/q1.go
@@ -1,32 +1,36 @@
 package main
 
-//各位之积 - 各位之和
-//https://leetcode.cn/problems/subtract-the-product-and-sum-of-digits-of-an-integer/
-import "fmt"
+//速算机器人
+//https://leetcode.cn/problems/nGK0Fy/
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	n := 62
-	r := subtractProductAndSum(n)
+	c := "AB"
+	r := calculate1(c)
 	fmt.Printf("%v", r)
 }
-func subtractProductAndSum(n int) int {
-	x, y := 0, 1
-	if n == 0 {
-		return 0
-	}
-	for {
-		if n == 0 {
-			break
+
+func calculate(s string) int {
+	x, y := 1, 0
+	for _, s1 := range s {
+		if string(s1) == "A" {
+			x = 2*x + y
+		} else {
+			y = 2*y + x
 		}
-		temp := n % 10
-
-		x += temp
-		y *= temp
-		n = n / 10
 	}
-
-	return y - x
+	return x + y
+}
+func calculate1(s string) int {
+	n := len(s)
+	r := math.Pow(2, float64(n))
+	return int(r)
 }
 
 //解题思路:
-//无需思路  拆成各个位的值 直接 加和 积和
+//不须遍历直接计算的话  可以拆开看 A  x = 2x + y   这时候计算 总值   y不变  x = x + (x+y) 即  x+y = 2(x+y)
+//不须遍历直接计算的话  可以拆开看 B  y = x + 2y   这时候计算 总值   x不变  y = y + (x+y) 即  x+y = 2(x+y)
+//所以 AB 没差别  只要看 操作式长度   即可 1 * 2^len(c)
--- 9.6/q3/q3.go
+++ 9.6/q3/q3.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"math"
+)
 
+//求是否为2的幂
+//https://leetcode.cn/problems/power-of-two/
+func main() {
+	n := 222
+	r := isPowerOfTwo(n)
+	fmt.Printf("%v", r)
 }
+func isPowerOfTwo(n int) bool {
+	i := 0
+	for {
+		temp := math.Pow(2, float64(i))
+		if int(temp) == n {
+			return true
+		}
+		if int(temp) > n {
+			return false
+		}
+		i++
+	}
+	return true
+}
+
+//题解思路  每次乘2  看是否超过目标值了
