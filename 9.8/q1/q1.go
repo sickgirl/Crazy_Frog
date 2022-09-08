e45ccaa25dafea31930e41470264f7eac25f954d
mountain <hot007boy@126.com>
2022-09-08 23:16:49 +0800

    上班卷

--- 9.8/q1/q1.go
+++ 9.8/q1/q1.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+//唯一摩尔斯密码词
+//https://leetcode.cn/problems/unique-morse-code-words/
+import "fmt"
 
+func main() {
+	s := []string{"a", "b", "c", "d"}
+
+	var r = uniqueMorseRepresentations(s)
+	fmt.Printf("%v ", r)
+}
+func uniqueMorseRepresentations(words []string) int {
+	types := []string{".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."}
+	res := map[string]bool{}
+	for _, v := range words {
+		temp := ""
+		for _, v1 := range v {
+			key1 := v1 - 'a'
+			value1 := types[key1]
+			temp += value1
+		}
+		res[temp] = true
+	}
+
+	return len(res)
+	//types := []string{".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."}
 }
--- 9.8/q2/q2.go
+++ 9.8/q2/q2.go
@@ -1,5 +1,47 @@
 package main
 
+// 删除中间节点
+//https://leetcode.cn/problems/delete-middle-node-lcci/
 func main() {
-
+	link := &ListNode{
+		Val: 1,
+		Next: &ListNode{
+			Val: 2,
+			Next: &ListNode{
+				Val: 3,
+				Next: &ListNode{
+					Val: 4,
+					Next: &ListNode{
+						Val:  5,
+						Next: nil,
+					},
+				},
+			},
+		},
+	}
+	node1 := link.Next.Next
+	deleteNode(node1)
 }
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func deleteNode(node *ListNode) {
+	if node != nil {
+		if node.Next == nil {
+			node = nil
+		} else {
+			node.Val = node.Next.Val
+			node.Next = node.Next.Next
+		}
+	}
+}
+
+func deleteNode1(node *ListNode) {
+	node.Val = node.Next.Val
+	node.Next = node.Next.Next
+}
+
+//题解 : 多联系链表
