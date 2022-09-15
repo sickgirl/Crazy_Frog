b3271fe42c88d17d69bbc02f3c82655d2518ad5d
mountain <hot007boy@126.com>
2022-09-16 00:22:42 +0800

    周四卷

--- 9.15/today/q1/q1.go
+++ 9.15/today/q1/q1.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import "fmt"
 
+//反转字符串中的单词 III
+//https://leetcode.cn/problems/reverse-words-in-a-string-iii/
+func main() {
+	s := "Let's take LeetCode contest"
+	res := reverseWords(s)
+	fmt.Printf("%v", res)
+}
+
+func reverseWords(s string) string {
+	res := ""
+	temp := ""
+	for _, v := range s {
+		if v != ' ' {
+			temp = string(v) + temp
+		} else {
+			res = res + temp + " "
+			temp = ""
+		}
+	}
+	res = res + temp
+	return res
 }
--- 9.15/today/q2/q2.go
+++ 9.15/today/q2/q2.go
@@ -1,5 +1,41 @@
 package main
 
-func main() {
+import "fmt"
 
+//541. 反转字符串 II
+//https://leetcode.cn/problems/reverse-string-ii/
+func main() {
+	s := "abcdefg"
+	k := 2
+	r := reverseStr(s, k)
+	fmt.Printf("%v", r)
+}
+
+func reverseStr(s string, k int) string {
+	res := ""
+	for {
+		n := len(s)
+		if n < k {
+			res = res + Bytes(s)
+			break
+		} else if n < 2*k {
+			res = res + Bytes(s[:k]) + s[k:]
+			break
+		}
+
+		tempS := s[:k]
+		tempS = Bytes(tempS)
+		temp2 := s[k : 2*k]
+		res = res + tempS + temp2
+		s = s[2*k:]
+	}
+	return res
+}
+
+func Bytes(s string) string {
+	r := make([]byte, len(s))
+	for i := 0; i < len(s); i++ {
+		r[i] = s[len(s)-i-1]
+	}
+	return string(r)
 }
