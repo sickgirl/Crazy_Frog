74d4e63e0eb9fc51c21cb95906e254ba0431a364
mountain <hot007boy@126.com>
2022-09-26 13:47:31 +0800

    周一学习

--- 9.26/today/q3/q3.go
+++ 9.26/today/q3/q3.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"math"
+)
 
+//剑指 Offer 17. 打印从1到最大的n位数
+//https://leetcode.cn/problems/da-yin-cong-1dao-zui-da-de-nwei-shu-lcof/
+func main() {
+	num := 1
+	r := printNumbers(num)
+	fmt.Printf("%v", r)
+}
+func printNumbers(n int) []int {
+	num := math.Pow(10, float64(n))
+	num2 := int(num)
+	r := []int{}
+	for i := 1; i < num2; i++ {
+		r = append(r, i)
+	}
+	return r
 }
