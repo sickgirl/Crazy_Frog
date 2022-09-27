a84d5bf3dc66cf80e4c180aa8e767c883e9d86e5
mountain <hot007boy@126.com>
2022-09-27 09:55:49 +0800

    周二学习

---
+++ 9.27/review/q2/q2.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+//1295. 统计位数为偶数的数字
+//https://leetcode.cn/problems/find-numbers-with-even-number-of-digits/
+func main() {
+	nums := []int{12, 345, 2, 6, 7896}
+	r := findNumbers(nums)
+	fmt.Printf("%v", r)
+}
+func findNumbers(nums []int) int {
+	r := 0
+	for _, v := range nums {
+		temp := getNums(v)
+		if temp%2 == 0 {
+			r++
+		}
+	}
+	return r
+}
+
+func getNums(num int) int {
+	r := 0
+	for {
+		if num < 10 {
+			r++
+			break
+		}
+		num = num / 10
+		r++
+	}
+	return r
+}
