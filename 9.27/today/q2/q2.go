2ccb47b2e987176320c92c8e6a04fa839a653c88
mountain <hot007boy@126.com>
2022-09-27 14:22:16 +0800

    周二学习

--- 9.27/today/q2/q2.go
+++ 9.27/today/q2/q2.go
@@ -1,5 +1,39 @@
 package main
 
-func main() {
+import "fmt"
 
+//1700. 无法吃午餐的学生数量
+//https://leetcode.cn/problems/number-of-students-unable-to-eat-lunch/
+func main() {
+	//[1,1,1,0,0,1]
+	//[1,0,0,0,1,1]
+	students := []int{1, 1, 1, 0, 0, 1}
+	sandwiches := []int{1, 0, 0, 0, 1, 1}
+	res := countStudents(students, sandwiches)
+	fmt.Printf("%v", res)
+}
+
+func countStudents(students []int, sandwiches []int) int {
+	end := false
+	for _, v := range sandwiches {
+		n := len(students)
+		if end {
+			break
+		}
+		for {
+			if students[0] == v {
+				students = students[1:]
+				break
+			} else {
+				if n == 0 {
+					end = true
+					break
+				}
+				temp := students[0]
+				students = append(students[1:], temp)
+				n--
+			}
+		}
+	}
+	return len(students)
 }
