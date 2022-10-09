b38c233f97bed4e0a7b16cf866ca53e4b1399a67
mountain <hot007boy@126.com>
2022-10-09 10:31:28 +0800

    假期归来学习

--- 10.October/10.8/today/q2/q2.go
+++ 10.October/10.8/today/q2/q2.go
@@ -1,5 +1,32 @@
 package main
 
-func main() {
+import "fmt"
 
+//1725. 可以形成最大正方形的矩形数目
+//https://leetcode.cn/problems/number-of-rectangles-that-can-form-the-largest-square/
+func main() {
+	rectangles := [][]int{{5, 8}, {3, 9}, {5, 12}, {16, 5}}
+	r := countGoodRectangles(rectangles)
+	fmt.Printf("%v", r)
+}
+
+func countGoodRectangles(rectangles [][]int) int {
+	ans := map[int]int{}
+	max := 0
+	for _, v := range rectangles {
+		min := min(v[0], v[1])
+		ans[min]++
+		if min > max {
+			max = min
+		}
+	}
+	return ans[max]
+}
+
+func min(x int, y int) int {
+	if x > y {
+		return y
+	} else {
+		return x
+	}
 }
--- 10.October/10.8/today/q3/q3.go
+++ 10.October/10.8/today/q3/q3.go
@@ -1,5 +1,33 @@
 package main
 
-func main() {
+import "fmt"
 
+//1684. 统计一致字符串的数目
+//https://leetcode.cn/problems/count-the-number-of-consistent-strings/
+func main() {
+	allowed := "ab"
+	words := []string{"ad", "bd", "aaab", "baa", "badab"}
+	r := countConsistentStrings(allowed, words)
+	fmt.Printf("%v", r)
+}
+
+func countConsistentStrings(allowed string, words []string) int {
+	ans := map[rune]bool{}
+	r := 0
+	for _, v := range allowed {
+		ans[v] = true
+	}
+	for _, v := range words {
+		error1 := false
+		for _, v1 := range v {
+			if !ans[v1] {
+				error1 = true
+				break
+			}
+		}
+		if !error1 {
+			r++
+		}
+	}
+	return r
 }
