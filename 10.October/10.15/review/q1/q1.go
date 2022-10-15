61d328787ac24b9abd3145e6af7c8f6888fdbe49
mountain <hot007boy@126.com>
2022-10-15 23:30:56 +0800

    忙里偷闲学习

--- 10.October/10.15/review/q1/q1.go
+++ 10.October/10.15/review/q1/q1.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+//各位之积 - 各位之和
+//https://leetcode.cn/problems/subtract-the-product-and-sum-of-digits-of-an-integer/
+import "fmt"
 
+func main() {
+	n := 62
+	r := subtractProductAndSum(n)
+	fmt.Printf("%v", r)
+}
+func subtractProductAndSum(n int) int {
+	if n == 0 {
+		return 0
+	}
+
+	x, y := 0, 1
+	for {
+		if n == 0 {
+			break
+		}
+		temp := n % 10
+		x += temp
+		y *= temp
+		n = n / 10
+	}
+	return y - x
 }
--- 10.October/10.15/today/q2/q2.go
+++ 10.October/10.15/today/q2/q2.go
@@ -1,5 +1,33 @@
 package main
 
-func main() {
+import "fmt"
 
+//解密字符串
+//https://leetcode.cn/problems/decode-the-message/
+//中文翻译确实是有问题
+//key 意思就是对照表  "fffdsa..."  翻译成对照表其实是
+//{
+//f:a, d:b, s:c, a:d ...
+//}
+func main() {
+	key := "the quick brown fox jumps over the lazy dog"
+	msg := "vkbs bs t suepuv"
+	r := decodeMessage(key, msg)
+	fmt.Printf("%v", r)
+}
+
+func decodeMessage(key string, message string) string {
+	mp := ['z' + 1]byte{' ': ' '}
+	cur := byte('a')
+	for _, c := range key {
+		if mp[c] == 0 {
+			mp[c] = cur
+			cur++
+		}
+	}
+	s := []byte(message)
+	for i, c := range s {
+		s[i] = mp[c]
+	}
+	return string(s)
 }
