062352306ae945f42441649cdf9c8c80dacddcc2
mountain <hot007boy@126.com>
2022-10-18 23:57:53 +0800

    忙里偷闲学习

--- 10.October/10.18/review/q2/q2.go
+++ 10.October/10.18/review/q2/q2.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import "fmt"
 
+//2413. 最小偶倍数
+//https://leetcode.cn/problems/smallest-even-multiple/
+func main() {
+	n := 5
+	r := smallestEvenMultiple(n)
+	fmt.Printf("%v", r)
+}
+func smallestEvenMultiple(n int) int {
+	if n%2 == 0 {
+		return n
+	}
+	return 2 * n
 }
--- 10.October/10.18/today/q1/q1.go
+++ 10.October/10.18/today/q1/q1.go
@@ -1,5 +1,44 @@
 package main
 
+//1640. 能否连接形成数组
+//https://leetcode.cn/problems/check-array-formation-through-concatenation/
 func main() {
+	var arr []int = []int{49, 18, 16}
+	var pieces [][]int = [][]int{{16, 18, 49}}
+	r := canFormArray(arr, pieces)
+	println(r)
+}
+func canFormArray(arr []int, pieces [][]int) bool {
+	map1 := map[int][]int{}
+	n := len(arr)
+	rn := 0
+	for _, v := range pieces {
+		rn += len(v)
+		map1[v[0]] = v
+	}
+
+	if n != rn {
+		return false
+	}
+
+	for {
+		temp := arr[0]
+		if _, ok := map1[temp]; !ok {
+			return false
+		}
+		tempList := map1[temp]
+		for i, v := range tempList {
+			if arr[i] != v {
+				return false
+			}
+		}
+
+		arr = arr[len(tempList):]
+		if len(arr) == 0 {
+			return true
+		}
+
+	}
+	return true
 
 }
